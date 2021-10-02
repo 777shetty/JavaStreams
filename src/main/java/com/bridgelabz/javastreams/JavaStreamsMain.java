89300775dfea4b8b8b00fcac2d9d13cab0b7a190
777shetty <shetty.chetu7@gmail.com>
2021-10-02 23:38:26 +0530

    REFACTORED:Proper identification followed

--- src/main/java/com/bridgelabz/javastreams/JavaStreamsMain.java
+++ src/main/java/com/bridgelabz/javastreams/JavaStreamsMain.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
 public class JavaStreamsMain {
 	public static void main(String[] args) {
 		ArrayList<Integer> myNumberList = new ArrayList<Integer>();
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 15; i++)
 			myNumberList.add(i);
 		myNumberList.stream().forEach(n -> System.out.println("number is: " + n));
+		myNumberList.stream().forEach(n -> System.out.println("Number is: " + n));
 
 		Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
 		Predicate<Integer> isEvenFunction = n -> n > 0 && n % 2 == 0;
@@ -21,24 +21,29 @@ public class JavaStreamsMain {
 				.map(toDoubleFunction)
 				.collect(Collectors.toList());
 		System.out.println("Obtained double list is "+doubleList);
+		System.out.println("Obtained double list is: "+doubleList);
 
 		List<Integer> evenStreamList=myNumberList.stream().filter(isEvenFunction).collect(Collectors.toList());
-		System.out.println("Even number list is: "+evenStreamList);
+		List<Integer> evenStreamList1=myNumberList.stream()
+				.filter(isEvenFunction)
+				.collect(Collectors.toList());
+		System.out.println("Even number list is: "+evenStreamList1);
+
 		Integer firstEvenNumber=myNumberList.stream()
 				.filter(isEvenFunction)
 				.findFirst()
 				.orElse(null);
 		System.out.println("The first even number is :"+firstEvenNumber);
+		System.out.println("The first even number is: "+firstEvenNumber);
+
 		Integer minEvenNumber=myNumberList.stream()
 				.filter(isEvenFunction)
 				.min(Integer::compare).orElse(null);
 		System.out.println("Minimum even number is: "+minEvenNumber);
-
 		Integer maxEvenNumber = myNumberList.stream()
 				.filter(isEvenFunction)
 				.max(Integer::compare)
 				.orElse(null);
 		System.out.println("Maximum even number is: "+maxEvenNumber);
 	}
-
 }
